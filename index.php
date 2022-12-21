5b719af59df1d2fed31e542f4580c4b90f47b62f
hashtag-academy <admin@hashtag.net.ua>
2022-12-21 19:42:33 +0200

    Login

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+    $login = $_POST["login"]; // "admin"
+    $password = $_POST["password"]; // "111"
+    if ($login === "admin" && $password === "111") {
+        echo "Hello admin";
+    }
     echo "<p>Hello world</p>";
 ?>
