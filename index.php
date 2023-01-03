ef0c870ec3826664f8a790d3b1c7999c6db2f7eb
hashtag-academy <admin@hashtag.net.ua>
2023-01-03 17:33:48 +0200

    modified:   index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     $password = $_POST["password"]; // "111"
     if ($login === "admin" && $password === "111") {
         echo "Hello admin";
+    } else {
+        echo "<p>Hello world</p>";
     }
-    echo "<p>Hello world</p>";
 ?>
